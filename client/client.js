4acd319403eb5b323fe475660fd88899b285bf37
David Beath <davidgbeath@gmail.com>
2013-09-12 15:20:13 +1200

    Added remove article

--- client/client.js
+++ client/client.js
@@ -7,8 +7,15 @@ Template.feeds.events({
         Meteor.call('addFeed', $('#feedUrl').val());
         $("#feedUrl").val("");
     },
-    'click input.remove': function(){
+    'click input.removeFeed': function(){
         Feeds.remove(this._id);
+    }
+    
+});
+
+Template.overview.events({
+    'click input.removeArticle': function(){
+        Articles.remove(this._id);
     }
 });
 
