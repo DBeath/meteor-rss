fba120acaf934ae2fd6478d6ae7e92e0e41e6047
David Beath <davidgbeath@gmail.com>
2013-11-28 23:17:34 +1300

    Moved function

--- client/client.js
+++ client/client.js
@@ -49,6 +49,19 @@ Template.feeds.events({
 });
 
 /// article ///
+function articleClick(article){
+    var articleOpen = Session.get('article_open');
+
+    Meteor.call('markRead', articleOpen, done);
+
+    if (articleOpen === article._id){
+        Session.set('article_open', null);
+    } else {
+        Session.set('previous_article', articleOpen);
+         
+        Session.set('article_open', article._id);   
+    };    
+};
 
 Template.article.open = function(){
     return Session.equals('article_open', this._id) ? "open" : "";
@@ -56,20 +69,9 @@ Template.article.open = function(){
 
 Template.article.events({
     'click .articletitle': function(){
-        var articleOpen = Session.get('article_open');
-        console.log(articleOpen);
-        Meteor.call('markRead', articleOpen, done);
-
-        if (articleOpen === this._id){
-            Session.set('article_open', null);
-        } else {
-            Session.set('previous_article', articleOpen);
-             
-            Session.set('article_open', this._id);   
-        };    
+        articleClick(this, done);
     },
     'click .markedRead': function(){
-        console.log('read');
         Meteor.call('markUnread', this._id, done);
     }
 });
