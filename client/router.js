1467fa05a14b004560542b62e3ba4dad53b0c603
David Beath <davidgbeath@gmail.com>
2013-12-04 13:11:55 +1300

    Router changes

--- client/router.js
+++ client/router.js
@@ -6,11 +6,20 @@ Router.configure({
 Router.map(function(){
 	this.route('home', {
 		path: '/',
-		template: 'home'
+		template: 'home',
+		yieldTemplates: {
+			'feedList': {to: 'feedList'},
+			'articleList': {to: 'articleList'}
+		}
 	});
 
 	this.route('article', {
 		path: '/:_id',
 		template: 'article'
 	});
+
+	this.route('feed', {
+		path: '/:title',
+		template: 'articleList'
+	})
 });
