00f9b237040a68881450958471706a47d533a870
David Beath <davidgbeath@gmail.com>
2014-05-11 22:07:00 +1200

    Updated style

--- client/router.js
+++ client/router.js
@@ -46,14 +46,8 @@ Router.map(function () {
 			return {
 				feedList: Feeds.find(),
 				articles: Articles.find({},
-        { sort:  [['read', 'asc'],["date", "desc"]] })
+        { sort: ["date", "desc"] })
 			};
 		}
-		// data: {
-			// feedList: Feeds.find(),
-			// articles: Articles.find({feedId: Session.get('current_feed')},
-   //      { sort:  [['read', 'asc'],["date", "desc"]] })
-			// articles: this.articles()
-		// }
 	});
 });
